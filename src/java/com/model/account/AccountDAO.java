ef94ffef423db8f4d1e5aa4747d0ed7670e6abf8
kat1002 <nguyenphuc10022004@gmail.com>
2024-01-12 10:31:50 +0700

    refractor user -> account

--- src/java/com/model/account/AccountDAO.java
+++ src/java/com/model/account/AccountDAO.java
@@ -2,54 +2,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.model.user;
+package com.model.account;
 
-import java.util.Date;
+import com.model.DAO;
+import java.util.List;
+import java.util.Optional;
 
 /**
  *
  * @author kat1002
  */
-public class User extends Account{
-    
-    private String fullName;
-    private String phoneNumber;
-    private String email;
-    private int gender;
-    private Date dob;
-    
-    public User(int id, String username, String password, int role) {
-        super(id, username, password, role);
-    }
-
-    public User( int id, String username, String password, int role, String fullName, String phoneNumber, String email, int gender, Date dob) {
-        super(id, username, password, role);
-        this.fullName = fullName;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.gender = gender;
-        this.dob = dob;
-    }
-    
-    
+public class AccountDAO implements DAO<Account> {
 
     @Override
-    public String getRole() {
+    public Optional<Account> get(long id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public String getPage() {
+    public List<Account> getAll() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public boolean checkRole(String url) {
+    public void insert(Account t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public String getButton() {
+    public void update(Account t, String[] params) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Account t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
--- src/java/com/model/account/User.java
+++ src/java/com/model/account/User.java
@@ -2,40 +2,54 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.model.user;
+package com.model.account;
 
-import com.model.DAO;
-import java.util.List;
-import java.util.Optional;
+import java.util.Date;
 
 /**
  *
  * @author kat1002
  */
-public class AccountDAO implements DAO<Account> {
+public class User extends Account{
+    
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private int gender;
+    private Date dob;
+    
+    public User(int id, String username, String password, int role) {
+        super(id, username, password, role);
+    }
+
+    public User( int id, String username, String password, int role, String fullName, String phoneNumber, String email, int gender, Date dob) {
+        super(id, username, password, role);
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.gender = gender;
+        this.dob = dob;
+    }
+    
+    
 
     @Override
-    public Optional<Account> get(long id) {
+    public String getRole() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public List<Account> getAll() {
+    public String getPage() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public void insert(Account t) {
+    public boolean checkRole(String url) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public void update(Account t, String[] params) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void delete(Account t) {
+    public String getButton() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
