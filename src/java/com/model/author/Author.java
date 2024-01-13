f6e75a9b1e84df0f62d6d49f656070742bbfe60c
Spicoder <134564075+Thang20041963@users.noreply.github.com>
2024-01-13 17:28:23 +0700

    Update 5 object-files (#14)
    - Add attribute of Author.java , Book.java , Discount.java , Order.java , OrderDetail.java
    - Add constructor of Author.java , Book.java , Discount.java , Order.java , OrderDetail.java
    - Add getter/setter of Author.java , Book.java , Discount.java , Order.java , OrderDetail.java

--- src/java/com/model/author/Author.java
+++ src/java/com/model/author/Author.java
@@ -9,5 +9,31 @@ package com.model.author;
  * @author kat1002
  */
 public class Author {
+    int id;
+    String name;
+
+    public Author() {
+    }
+
+    public Author(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
--- src/java/com/model/order/Order.java
+++ src/java/com/model/order/Order.java
@@ -4,10 +4,67 @@
  */
 package com.model.order;
 
+import java.util.Date;
+
 /**
  *
  * @author kat1002
  */
 public class Order {
-    
+    int id;
+    int customerId;
+    Date created;
+    String orderStatus;
+    int discountId;
+
+    public Order() {
+    }
+
+    public Order(int id, int customerId, Date created, String orderStatus, int discountId) {
+        this.id = id;
+        this.customerId = customerId;
+        this.created = created;
+        this.orderStatus = orderStatus;
+        this.discountId = discountId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public int getDiscountId() {
+        return discountId;
+    }
+
+    public void setDiscountId(int discountId) {
+        this.discountId = discountId;
+    }
 }
